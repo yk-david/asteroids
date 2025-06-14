86380720a6aef8efc588103316e530f81d744b0f
yk-david <k.yongkeon@gmail.com>
2025-06-14 21:41:17 +0900

    add game quit logic and set fps to 60

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-    while screen:
+    # Game loop
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color="black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
