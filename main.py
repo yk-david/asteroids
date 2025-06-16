b1d2aa53d9defcc478613d082e877e7f41c114bb
yk-david <k.yongkeon@gmail.com>
2025-06-16 21:31:34 +0900

    design player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -10,16 +11,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # Drawing game stage: order matters.
         pygame.Surface.fill(screen, color="black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
+
+    
+
+
 
 if __name__ == "__main__":
     main()
