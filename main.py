17c61f14761293b719e378e3a6f40f99284a430b
yk-david <k.yongkeon@gmail.com>
2025-06-16 23:54:50 +0900

    make groups

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # Game loop
@@ -18,10 +22,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        # Drawing game stage: order matters.
-        player.update(dt)
+        
+        for thing in updatable:
+            thing.update(dt)
         pygame.Surface.fill(screen, color="black")
-        player.draw(screen)
+        
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
